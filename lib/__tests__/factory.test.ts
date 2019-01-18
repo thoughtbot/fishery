65064dd0e4ab8c19603142174b6f8a8afe468ec9
Stephen Hanson <shanson@thoughtbot.com>
2019-01-18 11:28:10 -0600

    Add tests for buildList

--- lib/__tests__/factory.test.ts
+++ lib/__tests__/factory.test.ts
@@ -13,10 +13,19 @@ const userFactory = Factory.define<User>(({ sequence }) => {
   };
 });
 
-describe('Factory.build', () => {
+describe('factory.build', () => {
   it('creates the object', () => {
     const user = userFactory.build({ name: 'susan' });
     expect(user.id).not.toBeNull();
     expect(user.name).toEqual('susan');
+  });
+});
+
+describe('factory.buildList', () => {
+  it('creates a list of objects with the specified properties', () => {
+    const users = userFactory.buildList(2, { name: 'susan' });
+    expect(users.length).toBe(2);
+    expect(users[0].id).not.toEqual(users[1].id);
+    expect(users.map(u => u.name)).toEqual(['susan', 'susan']);
   });
 });
