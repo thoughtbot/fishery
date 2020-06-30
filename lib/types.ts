5130c800d1fa40c4637b8985b317b64a10057120
Brandon Houghton <brandon.houghton@greenlight.guru>
2020-06-30 18:32:36 -0400

    Modify DeepPartial to recurse for objects only
    Source: Oliver Joseph Ash, https://github.com/reduxjs/redux/pull/3369/
    files#diff-b52768974e6bc0faccb7d4b75b162c99

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,6 @@
-export type DeepPartial<T> = { [P in keyof T]?: DeepPartial<T[P]> };
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
+};
 export type GeneratorFnOptions<T, I> = {
   sequence: number;
   afterBuild: (fn: HookFn<T>) => any;
